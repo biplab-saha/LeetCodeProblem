8adb311fd6dbb9190f770f227671f082ddd8b498
Biplab Saha <biplab5523x@gmail.com>
2026-03-03 14:22:20 +0530

    medianSortedArrays

---
+++ Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+        int half = (m + n + 1) / 2;
+
+        int lo = 0, hi = m;
+
+        while (lo <= hi) {
+
+            int cut1 = (lo + hi) / 2;
+            int cut2 = half - cut1;
+
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (l1 <= r2 && l2 <= r1) {
+
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    return Math.max(l1, l2);
+                }
+
+            } else if (l1 > r2) {
+                hi = cut1 - 1;
+            } else {
+                lo = cut1 + 1;
+            }
+        }
+
+        return 0.0;
+    }
+}
